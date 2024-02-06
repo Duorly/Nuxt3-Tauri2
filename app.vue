94f1a838c440be4cd93d906c71745053de7cc1f5
Nebel MASS <nebel.mass@gmail.com>
2024-02-06 17:04:49 +0100

    INIT PROJECT

---
+++ app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div>
+      Hello World !!
+    </div>
+  </div>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+html, body {
+  background-color: rgba(100, 0, 0, 50%);
+  height: 100%;
+  overflow: hidden;
+}
+</style>
